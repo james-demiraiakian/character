7ba4dcd7fda0afea63a1ebe0b6cf922c475f908c
James Demiraiakian <james.armen@gmail.com>
2021-12-03 10:08:05 -0800

    Added snapshot test for Display.js

--- src/components/Display/Display.test.js
+++ src/components/Display/Display.test.js
@@ -1,8 +1,8 @@
 import { render } from '@testing-library/react';
-import Editor from './Editor';
+import Display from './Display';
 
 test('test of dom render', () => {
-  const container = render(<Editor />);
+  const container = render(<Display />);
 
   expect(container).toMatchSnapshot();
 });
